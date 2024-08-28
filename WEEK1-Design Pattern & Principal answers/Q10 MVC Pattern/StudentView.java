89e5f64aa508b925cf1d750f5f48522a263a07b3
Raji-27 <109898341+Raji-27@users.noreply.github.com>
2024-08-28 22:34:53 +0530

    Add files via upload

---
+++ WEEK1-Design Pattern & Principal answers/Q10 MVC Pattern/StudentView.java
@@ -0,0 +1,8 @@
+public class StudentView {
+    public void printStudentDetails(String studentName, String studentId, String studentGrade) {
+        System.out.println("Student: ");
+        System.out.println("ID: " + studentId);
+        System.out.println("Name: " + studentName);
+        System.out.println("Grade: " + studentGrade);
+    }
+}
